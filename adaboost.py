5a686274dbcd549aec4cbfc3ec049e3be46fb4ca
David Sackstein <davids@codeprecise.com>
2018-06-02 17:21:11 +0300

    adaboost with errors

--- adaboost.py
+++ adaboost.py
@@ -9,7 +9,9 @@ Author: Noga Zaslavsky
 Edited: Yoav Wald, May 2018
 
 """
+import numpy
 import numpy as np
+
 
 class AdaBoost(object):
 
@@ -22,14 +24,24 @@ class AdaBoost(object):
         """
         self.WL = WL
         self.T = T
-        self.h = [None]*T     # list of base learners
+        self.h = [None] * T  # list of base learners
         self.w = np.zeros(T)  # weights
 
     def train(self, X, y):
         """
         Train this classifier over the sample (X,y)
         """
+        m = X.shape[0]
+        D = np.asarray([1 / m] * m)
 
+        for t in range(self.T):
+            self.h[t] = self.WL(D, X, y)
+            e = self._curr_error(self.h[t], D, X, y)
+
+            self.w[t] = 0.5 * np.math.log(1 / e - 1) if e > 0.00000001 else 100
+            self._update_distribution(D, X, y, self.h[t], self.w[t])
+
+        return self.predict(X)
 
     def predict(self, X):
         """
@@ -38,9 +50,46 @@ class AdaBoost(object):
         y_hat : a prediction vector for X
         """
 
+        y_pred = [0]*X.shape[0]
+        for t in range(self.T):
+            y_pred_t = self.h[t].predict(X)
+            y_pred += y_pred_t * self.w[t]
+
+        return [1 if s > 0 else -1 for s in y_pred]
+
     def error(self, X, y):
         """
         Returns
         -------
         the error of this classifier over the sample (X,y)
         """
+
+        y_pred = self.predict(X)
+
+        sample_count = X.shape[0]
+        correct_predicts = 0
+        for i in range(sample_count):
+            correct_predicts += y_pred[i] * y[i]
+
+        errors = sample_count - correct_predicts
+        return errors / sample_count
+
+    def _update_distribution(self, D, X, y, h, w):
+        y_pred = h.predict(X)
+        denominator = 0
+        for i in range(len(X)):
+            denominator += self._weight(D, w, y, y_pred, i)
+
+        for i in range(len(X)):
+            D[i] = D[i] * self._weight(D, w, y, y_pred, i) / denominator
+
+    def _weight(self, D, w, y, y_pred, i):
+        return D[i] * np.math.exp(- w * y_pred[i] * y[i])
+
+    def _curr_error(self, h, D, X, y):
+        y_hat = h.predict(X)
+        total_error = 0
+        for i in range(y_hat.shape[0]):
+            error = 0 if y_hat[i] == y[i] else 1
+            total_error += D[i] * error
+        return total_error
--- ex4_runme.py
+++ ex4_runme.py
@@ -12,18 +12,81 @@ Date: May, 2018
 
 import numpy as np
 
-def Q3(): # AdaBoost
+from adaboost import AdaBoost
+from ex4_tools import decision_boundaries, h_opt, DecisionStump
+from matplotlib.pyplot import *
+
+
+def Q3():  # AdaBoost
     # TODO - implement this function
     return
 
-def Q4(): # decision trees
+
+def Q4():  # decision trees
     # TODO - implement this function
     return
 
-def Q5(): # spam data
+
+def Q5():  # spam data
     # TODO - implement this function
     return
+
+
+def to_ndarray(list_):
+    return np.array(list_).astype(np.float64)
+
+
+def read_all_data(file_name):
+    with open(file_name) as file_:
+        lines = file_.readlines()
+
+        data = []
+        labels = []
+
+        for line in lines:
+            values = line.split()
+            data.append(np.array(values[0:-1]))
+            labels.append(values[-1])
+
+    return to_ndarray(data), to_ndarray(labels)
+
+
+def get_data(test_size):
+    all_data, all_labels = read_all_data('spam.data')
+
+
+def _get_file_path(name):
+    return 'ex4files/SynData/{}.txt'.format(name)
+
+
+def _load_data(name):
+    return np.loadtxt(_get_file_path('X_' + name)), np.loadtxt(_get_file_path('y_' + name))
+
 
 if __name__ == '__main__':
+    X_train, y_train = _load_data('train')
+    X_val, y_val = _load_data('val')
+
+    T_values = range(5, 200, 5)
+    validation_error = []
+    training_error = []
+
+    for t in T_values:
+        ada_boost = AdaBoost(DecisionStump, t)
+        ada_boost.train(X_train, y_train)
+        validation_error.append(ada_boost.error(X_val, y_val))
+        training_error.append(ada_boost.error(X_train, y_train))
+
+    training_error_plot, = plot(T_values, training_error, linestyle='--', label='training_error')
+    validation_error_plot, = plot(T_values, validation_error, linestyle='-', label='validation_error')
+
+    legend(handles=[training_error_plot, validation_error_plot])
+
+    title('training and validation error vs T values')
+    xlabel('T values')
+    ylabel('training and validation error')
+    savefig('adaboost.png')
+    clf()
+
     # TODO - run your code for questions 3-5
     pass
